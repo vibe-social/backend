07a705c2bfa44387ba3fd67b3928cf9ef4e5b21f
Matic Conradi <matic.conradi@me.com>
2024-01-16 13:56:28 +0100

    Added docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { StripeService } from './stripe.service';
 import { SupabaseService } from './supabase.service';
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot({
+  envFilePath: '.env.local',
+});
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [StripeService, SupabaseService],
 })
